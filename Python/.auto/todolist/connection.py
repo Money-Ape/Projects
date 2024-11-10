909653ecbc7da6ff9d68967a956ed5934c890da5
Money-Ape <128079906+Money-Ape@users.noreply.github.com>
2024-11-11 00:40:47 +0530

    connection with hash successfully done.

--- Python/.auto/todolist/connection.py
+++ Python/.auto/todolist/connection.py
@@ -22,10 +22,16 @@ def server():
                 print(f"\033[1;32m{user}'s database connected successfully!\033[0m")
                 print(f"\033[1;32m'{schema}' has been selected as the default database.\033[0m")
                 return db_con
+
         except Error as err:
             print(f"\033[1;31mError: {err}\033[0m")
-            print("\033[1;31mInvalid username or password. Please try again.\033[0m")
-        finally:
-            if db_con and db_con.is_connected():
-                db_con.close()
-                print("\033[1;32mConnection successfully closed.\033[0m")
+            print("\033[1;31mInvalid Username or Password... Please try again.!\033[0m")
+
+            user, passcode = hashed.vu_input()
+            if not user or not passcode:
+                print("\033[1;31mFailed to verify credentials.!\nExiting...\033[0m")
+                break
+
+        if 'db_con' and db_con.is_connected():
+            db_con.close()
+            print("\033[1;32mConnection closed.!\033[0m")
