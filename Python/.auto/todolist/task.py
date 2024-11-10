4dc6576e1e8ae1c877d5db6f9365725ca45a0baa
Money-Ape <128079906+Money-Ape@users.noreply.github.com>
2024-11-10 16:34:20 +0530

    cursor created in task.py

--- Python/.auto/todolist/task.py
+++ Python/.auto/todolist/task.py
@@ -1,10 +1,16 @@
 import connection
 
-
-
 db_con=connection.server()
 
-
 if db_con and db_con.is_connected():
-    db_con.close()
-    print("\033[1;32mconnection successfully closed.!\033[0m")
+    try:
+        if db_con.is_connected():
+            with db_con.cursor() as cursor:
+                result = cursor.fetchone()
+                print("\033[1;32mconnected to database.! : \033[0m",result[0])
+    except Exception as err:
+        print(f"\033[1;31mError... Query execution failed.! : {err}\033[0m")
+
+    finally:
+        db_con.close()
+        print("\033[1;32mconnection successfully closed.!\033[0m")
